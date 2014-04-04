5fa3371c38ee9bb361a128b33e4d058e701c426d
Michael Thorne <michael.thorne@gmail.com>
2014-04-04 18:30:13 +0200

    Initialize EPG to specified start Prev/Next controls

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,45 @@ var components = {
         if (component_epg.length > 0)
         {
             /*
+             * Initialize
+             */
+
+            var epg_start = component_epg.data('start');
+            var epg_grid = $('.epg-grid', component_epg);
+            var epg_grid_scroll = 0;
+
+            if (typeof epg_start !== 'undefined' && epg_start !== '')
+            {
+                // Scroll the grid to the specified starting point
+                epg_grid.stop(true, true).animate({
+                    scrollLeft: epg_start - 120
+                }, 500);
+            }
+
+            /**
+             * Controls
+             */
+
+            var action_prev = $('[data-action="prev"]', component_epg);
+            var action_next = $('[data-action="next"]', component_epg);
+
+            /* Previous */
+            action_prev.on('click', function (e)
+            {
+                e.preventDefault();
+
+                scrollEPG(-Math.abs(240));
+            });
+
+            /* Next */
+            action_next.on('click', function (e)
+            {
+                e.preventDefault();
+
+                scrollEPG(240);
+            });
+
+            /*
              * Draggable
              */
 
@@ -40,11 +79,18 @@ var components = {
                 draggable.kinetic();
             }
 
-            /*
-             * Initialize
+            /**
+             * Scroll the EPG grid the specified distance
              */
 
-            var now = component_epg.data('now');
+            function scrollEPG(distance)
+            {
+                epg_grid_scroll = epg_grid.scrollLeft();
+
+                epg_grid.stop(true, true).animate({
+                    scrollLeft: epg_grid_scroll + distance
+                }, 500);
+            }
         }
     }
 };
